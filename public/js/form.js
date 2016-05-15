def6c50966d90d5c0e541d407d63957e06517cfa
Melanie <tardy.melanie.contact@gmail.com>
2016-05-15 10:45:57 +0200

    + Fix bugs

--- public/js/form.js
+++ public/js/form.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
+    // Duplication du champs .product-id
     $('.product-id').on('change', function () {
+        // Si le nbr d'élement ayant la classe product-id est inférieur à 2
         if ($('.product-id').length < 2) {
             var $select = $(this).parent(),
                 $newSelect = $select.clone(true);
@@ -8,12 +10,25 @@ $(document).ready(function() {
         }
     });
 
+    // Duplication du champs .sample-id
     $('.sample-id').on('change', function () {
+        // Si le nbr d'élement ayant la classe sample-id est inférieur à 10
         if ($('.sample-id').length < 10) {
             var $select = $(this).parent(),
                 $newSelect = $select.clone(true);
 
             $select.after($newSelect);
+        }
+    });
+
+    // Confirmation de réinitialisation
+    $('#btn-reset').on('click', function() {
+       var result = confirm('Etes-vous sûr de vouloir réinitilaiser le forumulaire ?');
+
+        // Si l'utilisateur réinitialise le formulaire, suppression des champs de produits dupliqués
+        if (result) {
+            $('.sample-id').first().parent().nextAll().remove();
+            $('.product-id').first().parent().nextAll().remove();
         }
     });
 });
